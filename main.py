f4ef59139e154721890ef1c6d5f15eb81cb7abce
riverry-prog <rmoratocardoso@gmail.com>
2025-09-24 16:36:32 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,54 @@
+#Multiplique 4 por 6 e divida o resultado por 2. (Concatene com a palavra resultado)
+multi = (4*6)/2
+print ('resultado',multi)
+
+#Calcule a soma de 5 e 7.(Concatene com a palavra resultado)
+soma = (5+7)
+print ('resultado', soma)
+
+#Eleve 3 à potência de 4.(Concatene com a palavra resultado)
+potência = (3**4)
+print ('resultado', potência)
+
+#Calcule a média de 8, 12 e 15.(Concatene com a palavra resultado)
+média = (8+12+15)/3
+print ('resultado', média)
+
+#Subtraia 10 de 2 e multiplique o resultado por 5.(Concatene com a palavra resultado)
+sub = (10-2)*5
+print ('resultado', sub)
+
+#Divida 27 por 3 e some 5.(Concatene com a palavra resultado)
+div = (27/3)+5
+print ('resultado', div)
+
+#Calcule o módulo ( % ) de 17 por 4.
+soma = (17%+4)
+
+#Multiplique 9 por 3 e, em seguida, eleve o resultado ao quadrado.
+multi = (9*3)**2
+
+
+#Calcule a raiz quadrada de 81.
+raiz = (81**2)
+
+#Adicione 20 a 3 vezes 4.
+adc = (20+3*4)  
+
+#Multiplique 15 por 2 e, em seguida, subtraia 7.
+multi = (15*2)-7
+
+#Eleve 5 ao cubo.
+int = (5**3)
+
+#Calcule a média de 17, 21 e 25.
+média = (17+21+25)/3
+
+#Multiplique 11 por 2 e adicione 7.
+multi = (11*2)+7
+
+#Subtraia 15 de 3 vezes 8 e divida o resultado por 2.
+sub = (15-3*8 /2)
+
+#Eleve 2 à potência de 10.
+potência = (10**2)
